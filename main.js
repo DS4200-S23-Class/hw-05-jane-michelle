3286b9f4739d9b05706015ee631e99bdfd8b53c7
Michelle Wang <michellewang0402@gmail.com>
2023-02-16 12:11:55 -0500

    structured scatter plot

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // set constants
-const FRAME_HEIGHT = 200;
+const FRAME_HEIGHT = 500;
 const FRAME_WIDTH = 500;
-const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
+const MARGINS = {left: 75, right: 75, top: 75, bottom: 75};
 
 const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
 const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.bottom;
@@ -16,42 +16,68 @@ function scatterPlot() {
 	// reading from scatter plot file
 	d3.csv("data/scatter-data.csv").then((data) => {
 
-		// const MAX_Y = d3.max(data, (d) => {return d.y;});
+		const MAX_Y = d3.max(data, (d) => {
+			return parseInt(d.y)
+		}) 
 
+		const Y_SCALE = d3.scaleLinear()
+							.domain([0, (MAX_Y)])
+							.range([VIS_HEIGHT, 0]);
 
-		// const Y_SCALE = d3.scaleLinear()
-		// 					.domain([0, (MAX_Y)])
-		// 					.range([VIS_HEIGHT, 0]);
+		const MAX_X = d3.max(data, (d) => {
+			return parseInt(d.x)
+		});
 
+		const X_SCALE = d3.scaleLinear()
+							.domain([0, (MAX_X)])
+							.range([0, VIS_WIDTH]);
 
-		// const MAX_X = d3.max(data, (d) => {return d;});
+		// Title
+        FRAME1.append("text")
+	        .attr("x", MARGINS.left + VIS_WIDTH/2)
+	        .attr("y", MARGINS.top - 25)
+	        .attr('text-anchor', "middle")
+	        .style("font-size", 18)
+	        .text("Scatter Plot");
+        
+        // X label
+        FRAME1.append("text")
+	        .attr("x", MARGINS.left + VIS_WIDTH/2)
+	        .attr("y", VIS_HEIGHT + 125)
+	        .attr("text-anchor", "middle")
+	        .style("font-size", 12)
+	        .text("x");
+        
+        // Y label
+        FRAME1.append("text")
+	        .attr("text-anchor", "middle")
+	        .attr("x", MARGINS.left - 50)
+	        .attr("y", VIS_HEIGHT - 100)
+	        .style("font-size", 12)
+	        .text("y");
 
-		// const X_SCALE = d3.scaleLinear()
-		// 					.domain([0, (MAX_X)])
-		// 					.range([0, VIS_WIDTH]);
-		// plot
-		FRAME1.selectAll("circle")
-			.data(data)
-			// loop through all the rows
-			.enter()
-			// for each row, append a circle 
-			.append("circle")
-				.attr("cx", (d) => {return d.x})
-				.attr("cy", (d) => {return d.y})
-				.attr("r", 10)
-				.attr("fill", (d) => {return d.color});
-
-		// add an axis
-		// g is used for a placeholder
 		FRAME1.append("g")
-				// attribute transform - moves horizontal and vertically
-				.attr("transform", 
-					// how far to translate horizontal (move over past the left margin) and vertical move past viz heights plus the margin
-					"translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")")
-				// call the axisBottom function and make an axis for the x_scale function and make 4 ticks
-				.call(d3.axisBottom(d.x).ticks(4))
-					// technically not in-line but you can also give it a class and style it in css
-					.attr("font-size", "20px");
+			.attr("transform", 
+				"translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")")
+			.call(d3.axisBottom(X_SCALE).ticks(9))
+				.attr("font-size", "10px");
+
+
+		FRAME1.append("g")
+			.attr("transform", 
+				"translate(" + MARGINS.left + "," + (MARGINS.bottom) + ")")
+			.call(d3.axisLeft(Y_SCALE).ticks(9))
+				.attr("font-size", "10px");
+
+        FRAME1.append("g")
+	        .selectAll("dot")
+	        .data(data)
+	        .enter()
+	        .append("circle")
+	        .attr("cx", (d) => {return X_SCALE(d.x) + MARGINS.left;})
+			.attr("cy", (d) => {return Y_SCALE(d.y) + MARGINS.top;})
+	        .attr("r", 4)
+	        .style("fill", "#CC0000");
 
 	});
 
